a99ede2140d58205b77f01df4678454bd4361cdf
Giselle Takahashi <giselle.takahashi@modmed.com>
2020-11-11 11:16:53 -0500

    EMA-142053: add SPM support

--- Sources/CLTokenInput/include/CLTokenInput.h
+++ Sources/CLTokenInput/include/CLTokenInput.h
@@ -16,8 +16,8 @@ FOUNDATION_EXPORT const unsigned char CLTokenInputVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CLTokenInput/PublicHeader.h>
 
-#import <CLTokenInput/CLTokenInputView.h>
-#import <CLTokenInput/CLTokenView.h>
-#import <CLTokenInput/CLTokenPillView.h>
-#import <CLTokenInput/CLToken.h>
-#import <CLTokenInput/CLBackspaceDetectingTextField.h>
+#import <CLTokenInputView.h>
+#import <CLTokenView.h>
+#import <CLTokenPillView.h>
+#import <CLToken.h>
+#import <CLBackspaceDetectingTextField.h>
